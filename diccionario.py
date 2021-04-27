ecec1b91acefca98ce8453fa0782cd6b0e3f7ae2
Fernando Sánchez Plascencia <fernandosanchezplacencia@gmail.com>
2021-04-26 23:48:00 -0500

    se modificaron las funciones y se logro generar nuevos registro en dict

--- diccionario.py
+++ diccionario.py
@@ -8,19 +8,33 @@ def registrar():
     nombre = input()
     ape_p = input()
     ape_m = input()
-    for clave in contactos.keys():
-        contactos["Nombre"]= nombre
-        contactos["Apellido_P"] = ape_p
-        contactos["Apellido_M"] = ape_m
+    lista_contactos.append({"Nombre":nombre,
+            "Apellido_P":ape_p,
+            "Apellido_M":ape_m})
     
+
+def buscar():
+    buscar = input()
+    for i in lista_contactos:
+        x=i.get("Nombre")
+        y=i.get("Apellido_P")
+        z=i.get("Apellido_M")
+        if buscar == x or buscar == y or buscar == z:
+            print("Este nombre existe")
+            break
+        else:
+            print("Este nombre no existe")
+            break
 
 def main():
     cont = 0
     while cont < 2:
         registrar()
         cont+=1
-    for clave, valor in contactos.items():
-        print(clave,valor,sep=":",end="\n")
+    for i in lista_contactos:
+        for clave,valor in i.items():
+            print(clave,valor)
+    buscar()
 
 
 if __name__ == "__main__":
